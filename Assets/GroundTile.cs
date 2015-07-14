cb5b2320a6cd2bb8def67ee980ae0454eb26ad79
Cody Banman <crbanman@gmail.com>
2015-07-14 08:26:21 -0700

    Playing Around With Collisions

---
+++ Assets/GroundTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundTile : MonoBehaviour {
+
+	bool occupied = false;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Entered");
+		occupied = true;
+	}
+	void OnTriggerExit2D(Collider2D other) {
+		Debug.Log ("Exited");
+		occupied = false;
+	}
+	
+	public bool isWalkable() {
+		return !occupied;
+	}
+}
--- Assets/Map.cs
+++ Assets/Map.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class Map : MonoBehaviour {
 
+	public static Map instance;
+
 	public static float xMax = 0;
 	public static float yMax = 0;
 
 	public static int width;
 	public static int height;
 
-	public static Tile[,] walkableTiles;
+	public static GroundTile[,] walkableTiles;
 
 	// Use this for initialization
 	void Awake () {
+		if(instance == null) {
+			instance = this;
+		}
 		IdentifyMaxPosition();
 		IdentifyWalkableTiles ();
 
@@ -30,16 +35,17 @@ public class Map : MonoBehaviour {
 		Debug.Log ("Max Position is (" + xMax + ", " + yMax + ")");
 	}
 
-	void IdentifyWalkableTiles () {
-		walkableTiles = new Tile[width, height];
+	static void IdentifyWalkableTiles () {
+		walkableTiles = new GroundTile[width, height];
 		
-		foreach (Transform child in transform) {
-			Tile tile = child.gameObject.GetComponent<Tile>();
+		foreach (Transform child in instance.transform) {
+			GroundTile tile = child.gameObject.GetComponent<GroundTile>();
 			walkableTiles[(int) tile.transform.position.x, (int) tile.transform.position.y] = tile;
 		}
 	}
 
 	public static bool IsTileWalkable(float x, float y) {
+		//IdentifyWalkableTiles ();
 		if(x < 0 || y < 0 || x > xMax || y > yMax || walkableTiles[(int) x, (int) y] == null) {
 			return false;
 		}
